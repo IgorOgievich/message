5b6c225a6860aa1003115858cc76267659515abc
IgorOgievich <igorboog@mail.ru>
2020-03-08 12:34:57 +0300

    init commit

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+        <h1>Igor Ogievich</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Message/Message.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './Message.css';
+
+function Message() {
+  return (
+    <div className="message">
+        <div>
+      My Message djsklgfksdjfkldsklnfkldsnlkgflkdsnfl
+        </div>
+    </div>
+  );
+}
+
+export default Message;
