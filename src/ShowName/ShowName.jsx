dcb7e2a9e7a689caba49e548d71963aa56595b24
IgorOgievich <igorboog@mail.ru>
2020-03-23 14:25:56 +0300

    Add ShowName

--- src/ShowName/ShowName.jsx
+++ src/ShowName/ShowName.jsx
@@ -26,8 +26,8 @@ class ShowName extends React.Component {
         return (
             <div className={a.showName}>
                 <p>Ваше имя:</p>
-                <input ref={newNumber} value="0"/>
-                <input type="text" ref={newName}/>
+                <input ref={newNumber} value="0" size={2}/>
+                <input type="text" ref={newName} placeholder="Введите ваше имя"/>
                 <button onClick={rezult}>Клик</button>
             </div>
         );
