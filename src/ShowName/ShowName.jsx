288cc6f5dc150e72d3ead857016ebe24117691fd
IgorOgievich <igorboog@mail.ru>
2020-03-26 17:34:55 +0300

    add function newarr;

--- src/ShowName/ShowName.jsx
+++ src/ShowName/ShowName.jsx
@@ -5,47 +5,57 @@ import DisplayName from "./DisplayName/DisplayName";
 
 class ShowName extends React.Component {
 
+    newName = React.createRef();
+    newNumber = React.createRef();
+    state = {
+        arr: [
+            {name: "Igor"},
+        ]
+    };
+
+    newarr = () =>{
+        let newNameOne = this.newName.current.value;
+        let twoName = {name: newNameOne};
+    let newNamer = [...this.state.arr, twoName];
+        this.setState({
+            arr: newNamer
+        });
+    };
+
     render = () => {
-        let newName = React.createRef();
-        let newNumber = React.createRef();
 
         let returnName = () => {
-            let name = newName.current.value;
+            let name = this.newName.current.value;
             alert("Привет " + `${name}`);
-            newName.current.value = "";
+            this.newName.current.value = "";
         };
 
         let plusOne = () => {
-            let plus = newNumber.current.value;
-            return (
-                newNumber.current.value = +plus + 1
+            let plus = this.newNumber.current.value;
+            this.newNumber.current.value = +plus + 1;
+            return(
+                plus
             )
+
         };
-
-
-        let state = [
-            {name: "Igor"},
-            {name: "Anna"},
-            {name: "Anna"},
-        ];
 
         let rezult = () => {
-            returnName()
-            plusOne()
+            this.newarr();
+            returnName();
+            plusOne();
         };
 
-
-        let arrName = state.map(e =>  <DisplayName name = {e.name} />);
-
+        let arrName = this.state.arr.map(e =>  <DisplayName name = {e.name} />);
 
         return (
             <div className={a.showName}>
                 <p>Ваше имя:</p>
-                <input ref={newNumber} value="0" size={2}/>
-                <input type="text" ref={newName} placeholder="Введите ваше имя"/>
+                <input ref={this.newNumber} value="0" size={2}/>
+                <input type="text" ref={this.newName} placeholder="Введите ваше имя"/>
                 <button onClick={rezult}>Клик</button>
                 <p>Введенные имена:</p>
                 {arrName}
+                {this.rename}
             </div>
         );
     };
