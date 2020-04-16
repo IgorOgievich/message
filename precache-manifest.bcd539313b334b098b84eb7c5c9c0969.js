8db049bf8da5e3ef4dfda54edd4b4acd3538b9ea
IgorOgievich <igorboog@mail.ru>
2020-04-16 15:55:08 +0300

    Updates

---
+++ precache-manifest.bcd539313b334b098b84eb7c5c9c0969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a8579c993bb6f0274a9f7cc716a8fd",
+    "url": "/message/index.html"
+  },
+  {
+    "revision": "849d21a5cc4be53ed741",
+    "url": "/message/static/css/main.e48abb96.chunk.css"
+  },
+  {
+    "revision": "98e2cc4eb429976ac10e",
+    "url": "/message/static/js/2.db85a9a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/message/static/js/2.db85a9a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849d21a5cc4be53ed741",
+    "url": "/message/static/js/main.33e6192c.chunk.js"
+  },
+  {
+    "revision": "fc2903bb68be2b73c59c",
+    "url": "/message/static/js/runtime-main.f6abf429.js"
+  }
+]);
